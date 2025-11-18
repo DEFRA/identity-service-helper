33c4efbd1172cfc2c55544f3a9127415df0726d1
Gary Woodfine <annex-storage-slip@duck.com>
2025-11-18 15:33:08 +0000

    Update `KrdsSyncLog` entity with new properties and configure `Database.Tests` project settings

--- src/Database/Entities/KrdsSyncLog.cs
+++ src/Database/Entities/KrdsSyncLog.cs
@@ -9,13 +9,17 @@ using Microsoft.AspNetCore.Identity;
 
 public class KrdsSyncLog : BaseProcessingEntity
 {
-   
     public Guid CorrelationId { get; set; }
+
     public string Upn { get; set; }
+
     public string PayloadSha256 { get; set; }
+
     public string SourceEndpoint { get; set; }
+
     public int HttpStatus { get; set; }
+
     public bool ProcessedOk { get; set; }
+
     public string Message { get; set; }
-  
 }
