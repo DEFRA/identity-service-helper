b10e4a91ae6fe782ff6887e4b5c18a14bf6d612c
Gary Woodfine <annex-storage-slip@duck.com>
2025-11-19 08:50:42 +0000

    Refactor entity configurations to improve readability and formatting consistency.

--- src/Database/Configuration/Base/BaseProcessingEntityConfiguration.cs
+++ src/Database/Configuration/Base/BaseProcessingEntityConfiguration.cs
@@ -7,24 +7,24 @@ namespace Livestock.Auth.Database.Configuration.Base;
 using Livestock.Auth.Database.Entities.Base;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public abstract class BaseProcessingEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity: BaseProcessingEntity
+public abstract class BaseProcessingEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+    where TEntity : BaseProcessingEntity
 {
     public virtual void Configure(EntityTypeBuilder<TEntity> builder)
     {
-        builder.ToTable(typeof (TEntity).Name.ToSnakeCase());
+        builder.ToTable(typeof(TEntity).Name.ToSnakeCase());
         builder.Property(x => x.Id)
             .HasColumnName(nameof(BaseProcessingEntity.Id).ToSnakeCase())
             .HasColumnType(ColumnTypes.UniqueIdentifier);
-        
+
         builder.Property(x => x.ReceivedAt)
             .HasColumnName(nameof(BaseProcessingEntity.ReceivedAt).ToSnakeCase())
             .HasColumnType(ColumnTypes.Timestamp)
             .HasDefaultValueSql("now()");
-       
+
         builder.Property(x => x.ProcessedAt)
             .HasColumnName(nameof(BaseProcessingEntity.ProcessedAt).ToSnakeCase())
             .HasColumnType(ColumnTypes.Timestamp)
             .HasDefaultValueSql("now()");
     }
-
 }
--- src/Database/Configuration/Base/BaseUpdateEntityConfiguration.cs
+++ src/Database/Configuration/Base/BaseUpdateEntityConfiguration.cs
@@ -7,24 +7,24 @@ namespace Livestock.Auth.Database.Configuration.Base;
 using Livestock.Auth.Database.Entities.Base;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public abstract class BaseUpdateEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity: BaseUpdateEntity
+public abstract class BaseUpdateEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+    where TEntity : BaseUpdateEntity
 {
     public virtual void Configure(EntityTypeBuilder<TEntity> builder)
     {
-        builder.ToTable(typeof (TEntity).Name.ToSnakeCase());
+        builder.ToTable(typeof(TEntity).Name.ToSnakeCase());
         builder.Property(x => x.Id)
             .HasColumnName(nameof(BaseUpdateEntity.Id).ToSnakeCase())
             .HasColumnType(ColumnTypes.UniqueIdentifier);
-        
+
         builder.Property(x => x.UpdatedAt)
             .HasColumnName(nameof(BaseUpdateEntity.UpdatedAt).ToSnakeCase())
             .HasColumnType(ColumnTypes.Timestamp)
             .HasDefaultValueSql("now()");
-       
+
         builder.Property(x => x.CreatedAt)
             .HasColumnName(nameof(BaseUpdateEntity.CreatedAt).ToSnakeCase())
             .HasColumnType(ColumnTypes.Timestamp)
             .HasDefaultValueSql("now()");
-        
     }
 }
