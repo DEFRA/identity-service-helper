f238dc4d87ae2291518e792b39316f61669049dd
Gary Woodfine <annex-storage-slip@duck.com>
2026-01-30 11:04:16 +0000

    Add initial EF Core migration and snapshot for `Postgres.Database`, introducing new entities and configurations.

---
+++ src/Requests/Filters/ValidationFilter.cs
@@ -0,0 +1,33 @@
+namespace Defra.Identity.Requests.Filters;
+
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+public class ValidationFilter<T> : IEndpointFilter
+{
+    private readonly IValidator<T> _validator;
+
+    public ValidationFilter(IValidator<T> validator)
+    {
+        _validator = validator;
+    }
+
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        var arg = context.Arguments.OfType<T>().FirstOrDefault();
+
+        if (arg == null)
+        {
+            return Results.BadRequest("Invalid request.");
+        }
+
+        var validationResult = await _validator.ValidateAsync(arg);
+
+        if (!validationResult.IsValid)
+        {
+            return Results.ValidationProblem(validationResult.ToDictionary());
+        }
+
+        return await next(context);
+    }
+}
