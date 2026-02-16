68f2e0f409fbfd06e531a00f234a38f3043b9524
Gary Woodfine <annex-storage-slip@duck.com>
2026-02-16 08:19:10 +0000

    Add `GetApplicationById` and `GetApplications` query models for application retrieval functionality.

---
+++ src/Requests/Applications/Queries/GetApplicationById.cs
@@ -0,0 +1,10 @@
+// <copyright file="GetApplicationById.cs" company="Defra">
+// Copyright (c) Defra. All rights reserved.
+// </copyright>
+
+namespace Defra.Identity.Requests.Applications.Queries;
+
+public class GetApplicationById
+{
+    public Guid Id { get; set; }
+}
