354852bbaac4fa078279398ab28a47e862ae29c1
Lee Moody <lee.moody@esynergy.co.uk>
2026-02-18 17:50:14 +0000

    Implement endpoint tests for none delegate or user associated cph endpoints

--- tests/Endpoint.Tests/Asserts/Should_be_200_OK_and_get_top_id.js
+++ tests/Endpoint.Tests/Asserts/Should_be_200_OK_and_get_top_id.js
@@ -6,21 +6,21 @@ client.test("Response content-type is json", () => {
 });
 
 // Should_be_200_OK.js (or your response handler file)
-client.test("Extract top user id", function () {
+client.test("Extract top item id", function () {
     // If your response is JSON array like: [{ id: 123, ... }, ...]
-    const users = response.body;
+    const items = response.body;
 
-    client.assert(Array.isArray(users), "Expected response to be an array of users");
-    client.assert(users.length > 0, "Expected at least one user");
+    client.assert(Array.isArray(items), "Expected response to be an array of items");
+    client.assert(items.length > 0, "Expected at least one item");
 
     // Pick "top" however you define it. Common options:
     // 1) first item:
-    const top = users[0];
+    const top = items[0];
 
     // 2) OR max id:
-    // const top = users.reduce((max, u) => (u.id > max.id ? u : max), users[0]);
+    // const top = users.reduce((max, u) => (u.id > max.id ? u : max), items[0]);
 
-    client.assert(top && top.id != null, "Expected user to have an id");
+    client.assert(top && top.id != null, "Expected item to have an id");
 
     // Store for the next request:
     client.global.set("id", String(top.id));
