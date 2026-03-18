e9d5f7fa76dde549f8f529f6e8f19660bbf351e3
Gary Woodfine <annex-storage-slip@duck.com>
2026-03-18 13:56:24 +0000

    Remove `ClientId` property from `Application` model, replace with `Id`, and update related services, tests, and validators accordingly.

--- src/Responses/Applications/Application.cs
+++ src/Responses/Applications/Application.cs
@@ -10,8 +10,6 @@ public class Application
 
     public string Name { get; set; } = string.Empty;
 
-    public Guid ClientId { get; set; }
-
     public string TenantName { get; set; } = string.Empty;
 
     public string Description { get; set; } = string.Empty;
