18edddfbfc0c1d71476f21b31196fa82d6e206f9
Cedric Brasey <cedric.brasey@plaet-side.co.uk>
2026-03-20 10:30:31 +0000

    Added new util do database interaction

---
+++ src/Database/DataSeeder/Program.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="Program.cs" company="Defra">
+// Copyright (c) Defra. All rights reserved.
+// </copyright>
+
+namespace Defra.Identity.DataSeeder;
+
+using System.CommandLine;
+using Defra.Identity.DataSeeder.Commands;
+
+internal static class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        RootCommand rootCommand = new("Database utilities for Defra Identity.");
+        rootCommand.Add(new RunScriptCommand());
+
+        ParseResult parseResult = rootCommand.Parse(args);
+        return await parseResult.InvokeAsync();
+    }
+}
