22ceb996bc615a934e25aed1db4af408924733bc
Lee Moody <lee.moody@esynergy.co.uk>
2026-06-19 11:31:56 +0100

    Apply correct commentary around operator id and context services

--- tests/Services.Tests/Common/Context/OperatorContextTests.cs
+++ tests/Services.Tests/Common/Context/OperatorContextTests.cs
@@ -12,31 +12,34 @@ public class OperatorContextTests
     [Fact]
     public void Should_Return_Operator_Id_From_Operator_Id_Service()
     {
+        // Arrange
         var operatorIdService = new OperatorIdService();
-
         var operatorId = Guid.NewGuid();
 
         operatorIdService.OperatorId = operatorId;
 
+        // Act
         var operatorContext = new OperatorContext(operatorIdService);
 
+        // Assert
         operatorContext.OperatorId.ShouldBe(operatorId);
     }
 
     [Fact]
     public void Should_Propagate_Invalid_Operation_Exception_When_Operator_Id_Not_Set()
     {
+        // Arrange
         var operatorIdService = new OperatorIdService();
-
         var operatorContext = new OperatorContext(operatorIdService);
 
+        // Act
         var act = () =>
         {
             var x = operatorContext.OperatorId;
-
             x.ShouldNotBe(Guid.Empty);
         };
 
+        // Assert
         var exception = act.ShouldThrow<InvalidOperationException>();
 
         exception.Message.ShouldBe("Operator id has not been set");
